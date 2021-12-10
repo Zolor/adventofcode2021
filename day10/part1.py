7ed69c9d90b2211bb908a2abc1d5796613df91f2
Erika Forslöf <erikaforslov@hotmail.com>
2021-12-10 14:10:33 +0100

    Prettify day10

--- day10/part1.py
+++ day10/part1.py
@@ -1,7 +1,7 @@
 data = open("input.txt").read().split("\n")
 
 bonkers = []
-
+result = 0
 for line in data:
     openers = []
     for l in line:
@@ -11,18 +11,14 @@ for line in data:
             if (l == ")" and openers[-1] == "(") or (l == "]" and openers[-1] == "[") or (l == "}" and openers[-1] == "{") or (l == ">" and openers[-1] == "<"):
                 openers = openers[:-1]
             else:
-                bonkers.append(l)
+                if l == ")":
+                    result += 3
+                elif l == "]":
+                    result += 57
+                elif l == "}":
+                    result += 1197
+                elif l == ">":
+                    result += 25137
                 break
-
-result = 0
-for s in bonkers:
-    if s == ")":
-        result += 3
-    elif s == "]":
-        result += 57
-    elif s == "}":
-        result += 1197
-    elif s == ">":
-        result += 25137
 
 print(result)
