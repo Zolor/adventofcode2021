7c5fc7c3beee57f4599e0cf0dee4c99b97cbb6db
Erika Forslöf <erikaforslov@hotmail.com>
2021-12-14 22:12:54 +0100

    day14 part 1 done!

--- day14/part1.py
+++ day14/part1.py
@@ -2,5 +2,24 @@ data = open("testinput.txt").read().split("\n")
 
 dct = {line.split(" -> ")[0]:line.split(" -> ")[1] for line in data if "->" in line}
 word = data[0]
+checklist = list(set(line.split("-> ")[1] for line in data if "->" in line))
 
-print(word, dct)
+for _ in range(40):
+    tmpword = str()
+    for x, i in enumerate(word):
+        if x + 1 >= len(word):
+            tmpword += i
+            break
+        elif i + word[x+1] in dct.keys():
+            tmpword += i + dct.get(i + word[x+1])
+    word = tmpword
+
+maxA = 0
+minA = 0
+for c in checklist:
+    if maxA < word.count(c):
+        maxA = word.count(c)
+    if minA == 0 or minA > word.count(c):
+        minA = word.count(c)
+
+print(maxA - minA)
